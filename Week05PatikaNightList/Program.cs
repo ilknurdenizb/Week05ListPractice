778a8b9b1bc17e1cc71e156405a2828e66dda2c1
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-24 22:41:43 +0300

    Add project files.

---
+++ Week05PatikaNightList/Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> partyMembers = new List<string>(); // List to hold party members
+partyMembers.Add("Bülent Ersoy"); // Adding members to the party
+partyMembers.Add("Ajda Pekkan");
+partyMembers.Add("Ebru Gündeş");
+partyMembers.Add("Hadise");
+partyMembers.Add("Hande Yener");
+partyMembers.Add("Tarkan");
+partyMembers.Add("Funda Arar");
+partyMembers.Add("Demet Akalın");
+Console.WriteLine("** Davetliler **");
+for (int i = 0; i < partyMembers.Count; i++) // Loop through the list of party members
+{
+    
+    Console.WriteLine($"{i + 1} - {partyMembers[i]}"); // Display each member with their index
+}
